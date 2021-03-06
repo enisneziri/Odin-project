cbad63fa7a060a5d21a261f8486411a878f0a761
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-06 10:51:44 +0100

    Add files via upload

---
+++ BUILDING WITH BACKGROUNDS AND GRADIENTS/main.js
@@ -0,0 +1,11 @@
+//Designing the input field for search
+
+function focusIn(){
+	document.getElementById("search").style.width = "130px";
+	document.getElementById("search").style.transition = "2s";
+}
+
+function focusOut(){
+	document.getElementById("search").style.width = "95px";
+	document.getElementById("search").style.transition = "2s";
+}
